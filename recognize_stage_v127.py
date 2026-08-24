e0270e76fc97cf5df5d35726f3f243afa7ec8ef8
bomicodes <dkempt2@gmail.com>
2026-08-24 00:04:29 -0500

    Wire v14.7 facial plastics and general ENT depth batch

--- recognize_stage_v127.py
+++ recognize_stage_v127.py
@@ -25,6 +25,7 @@ from vignettes_v143 import VIGNETTES_V143
 from vignettes_v144 import VIGNETTES_V144
 from vignettes_v145 import VIGNETTES_V145
 from vignettes_v146 import VIGNETTES_V146
+from vignettes_v147 import VIGNETTES_V147
 
 
 def apply_recognize_blind_reveal_v127(items):
@@ -198,5 +199,6 @@ for _batch, _name in (
     (VIGNETTES_V144, "v14.4"),
     (VIGNETTES_V145, "v14.5"),
     (VIGNETTES_V146, "v14.6"),
+    (VIGNETTES_V147, "v14.7"),
 ):
     _merge_validated_challenges(_batch, _name)
