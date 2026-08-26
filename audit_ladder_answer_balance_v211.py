ba34d7649f64e68b0a6b35fa4bcfbe8eec5819bd
bomicodes <dkempt2@gmail.com>
2026-08-26 14:25:51 -0500

    Audit Head and Neck ladder pass 2 balance

--- audit_ladder_answer_balance_v211.py
+++ audit_ladder_answer_balance_v211.py
@@ -2,7 +2,7 @@
 from collections import Counter, defaultdict
 import runtime_entry as rt
 
-PREFIXES = ("v209_", "v210_", "v212_", "v213_", "v216_", "v218_", "v219_")
+PREFIXES = ("v209_", "v210_", "v212_", "v213_", "v216_", "v218_", "v219_", "v220_")
 failures=[]
 groups=defaultdict(list)
 for q in rt.data.CLINICAL_CHALLENGES_V119:
