d6c39df2e0af8b211840b1a815b64d0d15ae39d8
bomicodes <dkempt2@gmail.com>
2026-08-27 20:05:13 -0500

    v25.2 chain Laryngology foundation alignment before balancing

--- ladder_answer_balance_v211.py
+++ ladder_answer_balance_v211.py
@@ -30,6 +30,7 @@ from vignette_ladders_v248 import apply_learning_ladders_v248
 from vignette_ladders_v249 import apply_learning_ladders_v249
 from vignette_ladders_v251 import apply_learning_ladders_v251
 from vignette_ladders_v252 import apply_learning_ladders_v252
+from laryngology_foundation_alignment_v252 import apply_laryngology_foundation_alignment_v252
 
 TARGET_PREFIXES=("v209_","v210_","v212_","v213_","v216_","v218_","v219_","v220_","v221_","v222_","v223_","v224_","v225_","v227_","v228_","v231_","v233_","v234_","v235_","v236_","v237_","v238_","v239_","v241_","v242_","v243_","v244_","v245_","v246_","v247_","v248_","v249_","v251_","v252_")
 
@@ -86,6 +87,7 @@ def apply_ladder_answer_balance_v211(challenges):
     apply_learning_ladders_v249(challenges, data._v6_item_id)
     apply_learning_ladders_v251(challenges, data._v6_item_id)
     apply_learning_ladders_v252(challenges, data._v6_item_id)
+    apply_laryngology_foundation_alignment_v252(challenges, data._v6_item_id)
 
     groups=defaultdict(list)
     for q in challenges:
