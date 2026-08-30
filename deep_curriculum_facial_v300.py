d71e5d272bcf6e9634eba2934e29810c17f84dcb
bomicodes <dkempt2@gmail.com>
2026-08-30 11:00:39 -0500

    revert: remove redundant parathyroid v31.1 wiring

--- deep_curriculum_facial_v300.py
+++ deep_curriculum_facial_v300.py
@@ -5,7 +5,6 @@ from deep_curriculum_facial_v301 import apply_facial_frontal_sinus_rebuild_v301
 from deep_curriculum_facial_v303 import apply_facial_reanimation_rebuild_v303
 from deep_curriculum_rhinology_v304 import apply_rhinology_complications_rebuild_v304
 from deep_curriculum_peds_v310 import apply_congenital_hearing_rebuild_v310
-from deep_curriculum_parathyroid_v311 import apply_parathyroid_rebuild_v311
 
 
 def apply_facial_noe_rebuild_v300(data_module, app_module=None):
@@ -14,7 +13,6 @@ def apply_facial_noe_rebuild_v300(data_module, app_module=None):
     reanimation_result = apply_facial_reanimation_rebuild_v303(data_module, app_module)
     sinus_complications_result = apply_rhinology_complications_rebuild_v304(data_module, app_module)
     congenital_hearing_result = apply_congenital_hearing_rebuild_v310(data_module, app_module)
-    parathyroid_result = apply_parathyroid_rebuild_v311(data_module, app_module)
     return {
         "patched": noe_result.get("patched", []),
         "count": noe_result.get("count", 0),
@@ -22,5 +20,4 @@ def apply_facial_noe_rebuild_v300(data_module, app_module=None):
         "v303_facial_reanimation": reanimation_result,
         "v304_rhinology_complications": sinus_complications_result,
         "v310_congenital_hearing": congenital_hearing_result,
-        "v311_parathyroid": parathyroid_result,
     }
