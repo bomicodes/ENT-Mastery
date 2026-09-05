3cd1521aa9c86a61def8af2267422983f2582714
bomicodes <dkempt2@gmail.com>
2026-09-05 16:59:19 -0500

    Add Cervicofacial Flap to v20.11 exact semantic/source gate

--- audit_concept_check_task_alignment_v211.py
+++ audit_concept_check_task_alignment_v211.py
@@ -7,8 +7,9 @@ from concept_check_laser_energy_safety_v211 import COHORT_LASER_V211
 from concept_check_frontal_draf_v211 import COHORT_DRAF_V211
 from concept_check_four_gland_parathyroid_v211 import COHORT_PARATHYROID_V211
 from concept_check_local_flap_reconstruction_v211 import COHORT_LOCAL_FLAP_V211
+from concept_check_cervicofacial_flap_v211 import COHORT_CERVICOFACIAL_V211
 
-COHORT={**BASE_COHORT,**COHORT_LASER_V211,**COHORT_DRAF_V211,**COHORT_PARATHYROID_V211,**COHORT_LOCAL_FLAP_V211}
+COHORT={**BASE_COHORT,**COHORT_LASER_V211,**COHORT_DRAF_V211,**COHORT_PARATHYROID_V211,**COHORT_LOCAL_FLAP_V211,**COHORT_CERVICOFACIAL_V211}
 QIDS=tuple(COHORT)
 SEMANTIC_REQUIREMENTS={
  "cc-v112-rec-facial-plastics-trauma-facial-soft-tissue-lacerations-burns":{
@@ -75,6 +76,14 @@ SEMANTIC_REQUIREMENTS={
   "perfusion":(("pale",),("dusky",),("congested",),("hematoma",),("torsion",)),
   "rescue":(("release constricting",),("evacuate a hematoma",),("revise the inset","reopen")),
  },
+ "cc-v112-rec-facial-plastics-trauma-cervicofacial-flap":{
+  "indication":(("moderate-to-large","moderate to large"),("cheek",),("cervical",),("laxity",)),
+  "vector":(("lower eyelid",),("inferior",),("superolateral",),("ectropion",)),
+  "plane":(("superficial",),("deep",),("facial nerve",),("parotid",)),
+  "support":(("deep fixation",),("canthal support",)),
+  "perfusion":(("pale",),("dusky",),("hematoma",),("kink","twist")),
+  "bailout":(("regional flap",),("free tissue",),("bulk",),("lining","structural support")),
+ },
 }
 
 def _words(s): return len(re.findall(r"\b\w+[\w'-]*\b",str(s or '')))
@@ -119,6 +128,9 @@ def main():
     if required not in cites: failures.append('source_'+required+':'+qid)
   elif 'local-flap-reconstruction' in qid:
    pass
+  elif 'cervicofacial-flap' in qid:
+   for required in ('alam','sakellariou'):
+    if required not in cites: failures.append('source_'+required+':'+qid)
   else:
    for required in ('american burn association','braun'):
     if required not in cites: failures.append('source_'+required+':'+qid)
